0fd71d3d8adc8e0c5afaac0410c37b9310d254e1
Pavel Chocholouš <pavel@chocholous.net>
2024-06-06 12:55:47 +0000

    priprava na hodinu

--- app.py
+++ app.py
@@ -1,82 +1,32 @@
-from pathlib import Path
-import time
 import streamlit as st
 from PIL import Image
 
-# --- PATH SETTINGS ---
-current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
-css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "CV.pdf"
-profile_pic = current_dir / "assets" / "profile-pic.png"
-
 # --- GENERAL SETTINGS ---
-PAGE_TITLE = "Digital CV | John Doe"
-PAGE_ICON = ":wave:"
-NAME = "John Doe"
-DESCRIPTION = """
-Senior Data Analyst, assisting enterprises by supporting data-driven decision-making.
-"""
-EMAIL = "johndoe@email.com"
-SOCIAL_MEDIA = {
-    "OnlyFans": "https://onlyfans.com/chocho_work",
-    "LinkedIn": "https://www.linkedin.com/in/pavel-chocholous/",
-    "GitHub":
-    "https://github.com/pavel242242/digital-resume-template-streamlit",
-    "Twitter": "https://twitter.com/pavel242",
-}
-PROJECTS = {
-    "🏆 Sales Dashboard - Comparing sales across three stores":
-    "https://youtu.be/Sb0A9i6d320",
-    "🏆 Income and Expense Tracker - Web app with NoSQL database":
-    "https://youtu.be/3egaMfE9388",
-    "🏆 Desktop Application - Excel2CSV converter with user settings & menubar":
-    "https://youtu.be/LzCfNanQ_9c",
-    "🏆 MyToolBelt - Custom MS Excel add-in to combine Python & Excel":
-    "https://pythonandvba.com/mytoolbelt/",
-}
-SECTIONS = {
-    "Experience": "Exp",
-    "Tech stack": "Hard-skills",
-    "Career history": "CV",
-    "Portfolio": "Projects",
-}
+NAME = "Pavel Chocholous"
 
-st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
+st.set_page_config(page_title="Nadpis stránky", page_icon=":sunglasses:")
 
-# --- LOAD CSS, PDF & PROFIL PIC ---
-with open(css_file) as f:
-    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
-with open(resume_file, "rb") as pdf_file:
-    PDFbyte = pdf_file.read()
-profile_pic = Image.open(profile_pic)
-
+# -- TADY SE UDELA SIDE MENU
 with st.sidebar:
-    my_radio = st.radio("Wow effect radio button", list(SECTIONS.keys()))
-    awesomeness_level = st.slider("Awesomeness leves", 0, 10, 0)
+    my_radio = st.radio("Wow effect radio button", {
+        "Item 1": "Item 1 long description",
+        "Item 2": "Item 2 long description"
+    })
 
-# --- HERO SECTION ---
+# --- TADY DVA SLOUPECKY NAHORE NA STRANCE
 col1, col2 = st.columns(2, gap="small")
+
 with col1:
-    st.image(profile_pic, width=230)
+    st.title("Nějaký text")
+    st.write("Nějaký další text")
 
 with col2:
     st.title(NAME)
-    st.write(DESCRIPTION)
-    st.download_button(
-        label=" 📄 Download Resume",
-        data=PDFbyte,
-        file_name=resume_file.name,
-        mime="application/octet-stream",
-    )
-    st.write("📫", EMAIL)
+    st.write("Hehe")
 
-# --- SOCIAL LINKS ---
-st.write('\n')
-cols = st.columns(len(SOCIAL_MEDIA))
-for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
-    cols[index].write(f"[{platform}]({link})")
 
-if my_radio == "Experience":
+# --- A TADY SE MENI, CO JE POD NIMA, PODLE VYBRANEHO BUTTONKU V SIDE MENU
+if my_radio == "Item 1":
     # --- EXPERIENCE & QUALIFICATIONS ---
     st.write('\n')
     st.subheader("Experience & Qulifications")
@@ -87,7 +37,7 @@ if my_radio == "Experience":
     - ✔️ Excellent team-player and displaying strong sense of initiative on tasks
     """)
 
-if my_radio == "Tech stack":
+if my_radio == "Item 2":
     # --- SKILLS ---
     st.write('\n')
     st.subheader("Hard Skills")
@@ -97,52 +47,3 @@ if my_radio == "Tech stack":
     - 📚 Modeling: Logistic regression, linear regression, decition trees
     - 🗄️ Databases: Postgres, MongoDB, MySQL
     """)
-
-if my_radio == "Career history":
-    # --- WORK HISTORY ---
-    st.write('\n')
-    st.subheader("Work History")
-    st.write("---")
-
-    # --- JOB 1
-    st.write("🚧", "**Senior Data Analyst | Ross Industries**")
-    st.write("02/2020 - Present")
-    st.write("""
-    - ► Used PowerBI and SQL to redeﬁne and track KPIs surrounding marketing initiatives, and supplied recommendations to boost landing page conversion rate by 38%
-    - ► Led a team of 4 analysts to brainstorm potential marketing and sales improvements, and implemented A/B tests to generate 15% more client leads
-    - ► Redesigned data model through iterations that improved predictions by 12%
-    """)
-
-    # --- JOB 2
-    st.write('\n')
-    st.write("🚧", "**Data Analyst | Liberty Mutual Insurance**")
-    st.write("01/2018 - 02/2022")
-    st.write("""
-    - ► Built data models and maps to generate meaningful insights from customer data, boosting successful sales eﬀorts by 12%
-    - ► Modeled targets likely to renew, and presented analysis to leadership, which led to a YoY revenue increase of $300K
-    - ► Compiled, studied, and inferred large amounts of data, modeling information to drive auto policy pricing
-    """)
-
-    # --- JOB 3
-    st.write('\n')
-    st.write("🚧", "**Data Analyst | Chegg**")
-    st.write("04/2015 - 01/2018")
-    st.write("""
-    - ► Devised KPIs using SQL across company website in collaboration with cross-functional teams to achieve a 120% jump in organic traﬃc
-    - ► Analyzed, documented, and reported user survey results to improve customer communication processes by 18%
-    - ► Collaborated with analyst team to oversee end-to-end process surrounding customers' return data
-    """)
-
-if my_radio == "Portfolio":
-    # --- Projects & Accomplishments ---
-    st.write('\n')
-    st.subheader("Projects & Accomplishments")
-    st.write("---")
-    for project, link in PROJECTS.items():
-        st.write(f"[{project}]({link})")
-
-for i in range(awesomeness_level + 1):
-    awesomeness_level = i
-    with st.sidebar:
-        st.write("🎉" * i)
-    time.sleep(0.2)
